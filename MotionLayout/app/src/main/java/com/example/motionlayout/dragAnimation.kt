d02236e748d32cbafba9b59f2b8827c18ed58c94
sery270 <sery270@naver.com>
2021-03-22 19:01:41 +0900

    [FEAT] dragAnimation 연습

---
+++ MotionLayout/app/src/main/java/com/example/motionlayout/dragAnimation.kt
@@ -0,0 +1,11 @@
+package com.example.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dragAnimation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_animation)
+    }
+}
