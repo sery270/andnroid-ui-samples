7b9f0ff5cf99f363d0b7ea5c8778a5e5b1954548
sery270 <sery270@naver.com>
2021-03-25 23:14:21 +0900

    [CHORE] git 정리

--- MotionLayout/app/src/main/java/com/example/motionlayout/ImageFilterView.kt
+++ MotionLayout/app/src/main/java/com/example/motionlayout/ImageFilterView.kt
@@ -1,9 +1,15 @@
 package com.example.motionlayout
 
+import android.content.pm.ServiceInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.core.content.getSystemService
+import androidx.core.os.HandlerCompat.postDelayed
 
 class ImageFilterView : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_filter_view)
