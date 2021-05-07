478b97547113cfb6881b40aa671d8977cebc24ae
sery270 <sery270@naver.com>
2021-05-07 15:33:54 +0900

    [FEAT] onMeasure 작성

--- CustomView/FoldableView/app/src/main/java/com/example/foldableview/View/FoldableView.kt
+++ CustomView/FoldableView/app/src/main/java/com/example/foldableview/View/FoldableView.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.provider.SyncStateContract
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 class FoldableView @JvmOverloads constructor(
@@ -21,20 +23,34 @@ class FoldableView @JvmOverloads constructor(
         canvas.drawCircle(100F, 100F, 80F, pnt)
     }
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//
-//        // height
-//        var heightMode = MeasureSpec.getMode(heightMeasureSpec)
-//        var heightSize = 0
-//        when(heightMode){
-//            // wrap_content -> 필요로하는 크기 지정해주어야 함
-//            MeasureSpec.AT_MOST -> {}
-//            // match_parent, fill_parent
-//            MeasureSpec.EXACTLY -> {}
-//            // 특정한 크기 값이 설정되었을 때 (mode가 셋팅하지 않은 크기가 넘어올 때)
-//            MeasureSpec.UNSPECIFIED -> {}
-//        }
-//
-//        // width
-//    }
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+        // height
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        var heightSize = 0
+        when(heightMode){
+            // wrap_content -> 필요로하는 크기 지정해주어야 함
+            MeasureSpec.AT_MOST -> {heightSize = 200}
+            // match_parent, fill_parent
+            MeasureSpec.EXACTLY -> {heightSize = MeasureSpec.getSize(heightMeasureSpec)}
+            // 특정한 크기 값이 설정되었을 때 -> 설정받은 크기 넣어주면 됨
+            MeasureSpec.UNSPECIFIED -> {heightSize = heightMeasureSpec}
+        }
+
+        // width
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        var widthSize = 0
+        when(widthMode){
+            // wrap_content -> 필요로하는 크기 지정해주어야 함
+            MeasureSpec.AT_MOST -> {widthSize = 200}
+            // match_parent, fill_parent
+            MeasureSpec.EXACTLY -> {widthSize = MeasureSpec.getSize(widthMeasureSpec)}
+            // 특정한 크기 값이 설정되었을 때 -> 설정받은 크기 넣어주면 됨
+            MeasureSpec.UNSPECIFIED -> {widthSize = widthMeasureSpec}
+        }
+
+        Log.d("onMeasure", "widthSize : $widthSize && heightSize : $heightSize")
+
+        setMeasuredDimension(widthSize, heightSize)
+    }
 }
