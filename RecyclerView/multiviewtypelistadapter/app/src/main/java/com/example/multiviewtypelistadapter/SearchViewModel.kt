acb75e92f4f5fa60242fb6bca1422029406e827d
sery270 <sery270@naver.com>
2021-05-12 15:51:34 +0900

    [FEAT] SearchViewModel 작성

---
+++ RecyclerView/multiviewtypelistadapter/app/src/main/java/com/example/multiviewtypelistadapter/SearchViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.multiviewtypelistadapter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class SearchViewModel constructor(
+    private val searchRepository: SearchRepository
+) : ViewModel() {
+    private val _bookList = MutableLiveData<List<SearchItem.BookItem>>()
+    val bookList: LiveData<List<SearchItem.BookItem>>
+        get() = _bookList
+    private val _moimList = MutableLiveData<List<SearchItem.MoimItem>>()
+    val moimList: LiveData<List<SearchItem.MoimItem>>
+        get() = _moimList
+
+    fun getBookList() {
+        viewModelScope.launch {
+            _bookList.value = searchRepository.getBookList()
+        }
+    }
+
+    fun getMoimList() {
+        viewModelScope.launch {
+            _moimList.value = searchRepository.getMoimList()
+        }
+    }
+}
